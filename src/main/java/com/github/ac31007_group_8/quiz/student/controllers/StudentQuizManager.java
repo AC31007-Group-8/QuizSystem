30227f028204efd08b1ebd346f8e3763c5b67a95
Can <cantangafuroglu@yahoo.com>
2017-02-21 22:20:11 +0000

    Added quiz model. Questions and answers are now sent to the mustache view in JSON form. Added dependency for Apache Commons Lang. Added accessor method for question_id for the Question class in staff.store.

--- src/main/java/com/github/ac31007_group_8/quiz/student/controllers/StudentQuizManager.java
+++ src/main/java/com/github/ac31007_group_8/quiz/student/controllers/StudentQuizManager.java
@@ -1,14 +1,21 @@
 package com.github.ac31007_group_8.quiz.student.controllers;
 
+import com.github.ac31007_group_8.quiz.student.models.StudentQuizModel;
 import com.github.ac31007_group_8.quiz.util.Init;
 import spark.Request;
 import spark.Response;
 import spark.TemplateEngine;
 import spark.template.mustache.MustacheTemplateEngine;
 
-import java.util.HashMap;
+import java.util.*;
+import java.util.logging.Logger;
+
+import com.github.ac31007_group_8.quiz.student.*;
+import com.github.ac31007_group_8.quiz.staff.store.*;
 
 import static spark.Spark.*;
+import com.google.gson.*;
+import org.apache.commons.lang3.tuple.*;
 
 /**
  * @author Can G, Allan M
@@ -27,7 +34,27 @@ public class StudentQuizManager {
 
     public static Object serveTakeQuiz(Request req, Response res){
 
+        int quizID = 1;
+        String quizStr = req.queryParams("quizID");
+        Logger.getGlobal().info("got param value: " + quizStr);
+
+        StudentQuizModel quizModel = new StudentQuizModel();
+        List<Pair<Question, List<Answer>>> questionSets = quizModel.getQuestionSets(quizID);
+
+        Gson gson = new GsonBuilder().create();
+        String questionSetsJson = gson.toJson(questionSets);
+
         HashMap<String, Object> map = new HashMap<>();
+        map.put("quizID", quizStr);
+        map.put("questionSets", questionSetsJson);
+        map.put("testKey", "testVal");
+
+        //for (Pair<Question, List<Answer>> questionSet : questionSets) {
+        //
+        //    for (Answer answer : questionSet.getRight()) {
+        //
+        //    }
+        //}
 
         TemplateEngine eng = new MustacheTemplateEngine();
         return eng.render(eng.modelAndView(map, "student/takeQuiz.mustache"));
