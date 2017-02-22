d6b15e02bb1f75ddc8378649df447eba041db346
Kerr <Kerr@172.19.180.140>
2017-02-22 10:42:06 +0000

    Added some basic stuff for viewing student's results. ResultModel is empty atm, so this still uses the QuizModel

---
+++ src/main/resources/public/student/getResult.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Kerr on 22/02/2017.
+ */
+
+function startQuiz(){
+
+    //display results
+    document.getElementById("resultContainer").style.display = "block";
+
+}
+
+function goBack() {
+    //return to previous page (list of completeed quizzes)
+    //TODO: implement
+}
