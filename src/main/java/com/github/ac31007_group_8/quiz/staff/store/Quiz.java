aadefe64bc9c0dfc1595de5a824fdfdc0be0de44
Erik Jeny <Temp@Luna>
2017-02-22 23:53:17 +0000

    setter and getter methods

--- src/main/java/com/github/ac31007_group_8/quiz/staff/store/Quiz.java
+++ src/main/java/com/github/ac31007_group_8/quiz/staff/store/Quiz.java
@@ -5,6 +5,9 @@
  */
 package com.github.ac31007_group_8.quiz.staff.store;
 
+import java.util.ArrayList;
+import com.github.ac31007_group_8.quiz.staff.store.Question;
+
 /**
  *
  * @author Erik Jeny
@@ -16,6 +19,7 @@ public class Quiz {
     int time_limit;
     String module_id;
     String title;
+    ArrayList<Question> questions;
     boolean publish_status; 
     
     public Quiz(){
@@ -24,6 +28,7 @@ public class Quiz {
         time_limit = 0;
         module_id = "";
         title = "";
+        questions = null;
         publish_status = false;
     }
     
@@ -35,5 +40,66 @@ public class Quiz {
         this.title = title;
         this.publish_status = publish_status;
     }
+
+    public int getQuiz_id() {
+        return quiz_id;
+    }
+
+    public void setQuiz_id(int quiz_id) {
+        this.quiz_id = quiz_id;
+    }
+
+    public int getStaff_id() {
+        return staff_id;
+    }
+
+    public void setStaff_id(int staff_id) {
+        this.staff_id = staff_id;
+    }
+
+    public int getTime_limit() {
+        return time_limit;
+    }
+
+    public void setTime_limit(int time_limit) {
+        this.time_limit = time_limit;
+    }
+
+    public String getModule_id() {
+        return module_id;
+    }
+
+    public void setModule_id(String module_id) {
+        this.module_id = module_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public ArrayList<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(ArrayList<Question> questions) {
+        this.questions = questions;
+    }
+    
+    public void addQuestion(Question question) {
+        this.questions.add(question);
+    }
+
+    public boolean isPublish_status() {
+        return publish_status;
+    }
+
+    public void setPublish_status(boolean publish_status) {
+        this.publish_status = publish_status;
+    }
+    
     
 }
