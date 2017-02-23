837d5de0ab4f47407ad2de052007e59d1ad6b2ce
RONAN-HP\ronan <rljoy@dundee.ac.uk>
2017-02-23 15:48:20 +0000

    ResultsManager added to handle staff member viewng student results viewResults.mustache file to handle html

---
+++ src/main/java/com/github/ac31007_group_8/quiz/staff/controllers/ResultsManager.java
@@ -0,0 +1,59 @@
+package com.github.ac31007_group_8.quiz.student.controllers;
+
+import com.github.ac31007_group_8.quiz.student.models.StudentQuizModel;
+import com.github.ac31007_group_8.quiz.util.Init;
+import spark.Request;
+import spark.Response;
+import spark.TemplateEngine;
+import spark.template.mustache.MustacheTemplateEngine;
+
+import java.util.*;
+import java.util.logging.Logger;
+
+import com.github.ac31007_group_8.quiz.student.*;
+import com.github.ac31007_group_8.quiz.staff.store.*;
+
+import static spark.Spark.*;
+import com.google.gson.*;
+import org.apache.commons.lang3.tuple.*;
+/**
+ *
+ * @author ronan (adapted from Can, Allan and Vlad)
+ */
+public class ResultsManager {
+    
+    
+    public ResultsManager(){
+        
+        @Init
+    public static void init() {
+        get("/staff/viewResults", ResultsManager::serveViewResults);
+        post("/staff/viewResults", "application/json", ResultsManager::receiveViewResults);
+    }
+    
+    }
+    
+    
+    public static Object serveViewResults(Request req, Response res){
+        
+        //read parameter
+        int quizID = Integer.parseInt(req.queryParams("quizID"));
+        int studentID = Integer.parseInt(req.queryParams("studentID"));
+        studentID = 1; //Place holder no login atm
+
+        //specify values to be displayed on page:
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("quiz", quizID); //passing values to mustache to be displayed on the page by putting them in a hashmap
+        map.put("testKey", "testVal");
+
+        //render page:
+        TemplateEngine eng = new MustacheTemplateEngine();
+        return eng.render(eng.modelAndView(map, "staff/ViewResults.mustache"));
+
+
+    }
+    
+    public static Object receiveViewResults(Request req, Response res){
+    
+    }
+}
