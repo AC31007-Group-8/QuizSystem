3521f291920a4f25d36c6d6ca73d20bc7c5b3dae
Arkan Emberwalker <arkan@drakon.io>
2017-02-23 12:49:39 +0000

    More work on quiz stats.
    Also put back config example... Again.

--- src/main/java/com/github/ac31007_group_8/quiz/staff/controllers/QuizStats.java
+++ src/main/java/com/github/ac31007_group_8/quiz/staff/controllers/QuizStats.java
@@ -3,8 +3,11 @@ package com.github.ac31007_group_8.quiz.staff.controllers;
 import com.github.ac31007_group_8.quiz.Database;
 import com.github.ac31007_group_8.quiz.generated.tables.pojos.Answer;
 import com.github.ac31007_group_8.quiz.generated.tables.pojos.Question;
+import com.github.ac31007_group_8.quiz.generated.tables.pojos.Quiz;
 import com.github.ac31007_group_8.quiz.util.Init;
 import com.github.ac31007_group_8.quiz.util.Pair;
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.jooq.DSLContext;
@@ -16,6 +19,7 @@ import spark.HaltException;
 import spark.Request;
 import spark.Response;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
@@ -55,7 +59,7 @@ public class QuizStats {
             Map<Integer, Question> questions = qAndA.first;
             Map<Integer, Set<Answer>> answers = qAndA.second;
 
-            Result<Record> results = db.select()//RESULT.fields())
+            Result<Record> dbResults = db.select()//RESULT.fields())
                     .from(QUIZ)
                     .join(RESULT).on(QUIZ.QUIZ_ID.equal(RESULT.QUIZ_ID))
                     .join(RESULT_TO_ANSWER).on(RESULT.RESULT_ID.equal(RESULT_TO_ANSWER.RESULT_ID))
@@ -64,7 +68,27 @@ public class QuizStats {
                     .where(QUIZ.QUIZ_ID.equal(id))
                     .fetch();
 
-            throw halt(500, "Not Implemented");
+            Set<Quiz> quizzes = dbResults.into(Quiz.class).stream().collect(Collectors.toSet());
+            if (quizzes.size() == 0) {
+                throw halt(404, "{\"error\": \"no such quiz\"}");
+            }
+            Quiz quiz = quizzes.toArray(new Quiz[]{})[0];
+
+            // wtb type aliases
+            Set<com.github.ac31007_group_8.quiz.generated.tables.pojos.Result> results = dbResults
+                    .into(com.github.ac31007_group_8.quiz.generated.tables.pojos.Result.class)
+                    .stream().collect(Collectors.toSet());
+
+            int attemptsTotal = results.size();
+            int uniqueAttempts = results.stream()
+                    .collect(Collectors.toMap(
+                            com.github.ac31007_group_8.quiz.generated.tables.pojos.Result::getStudentId,
+                            Function.identity(), (a, b) -> a)).size();
+
+            List<QuestionStatsData> qStats = getQuestionStats(dbResults);
+
+            QuizStatsData stats = new QuizStatsData(quiz, qStats, attemptsTotal, uniqueAttempts);
+            return GSON.toJson(stats);
         } catch (HaltException ex) {
             // Pass!
             throw ex;
@@ -81,6 +105,7 @@ public class QuizStats {
         throw halt(500, "Not Implemented");
     }
 
+    // TODO: Refactor out into a model class.
     private static Pair<Map<Integer, Question>, Map<Integer, Set<Answer>>> getQuestionsAnswersForQuiz(DSLContext db,
                                                                                                       int quizId) {
         Result<Record> questionsAndAnswersResult = db.select()
@@ -99,6 +124,56 @@ public class QuizStats {
                 .collect(Collectors.groupingBy(Answer::getQuestionId, Collectors.toSet()));
 
         return new Pair<>(questions, answers);
+    }
+
+    private static List<QuestionStatsData> getQuestionStats(Result<Record> dbResults) {
+        Multiset<Question> correct = HashMultiset.create();
+        Multiset<Question> incorrect = HashMultiset.create();
+
+        dbResults.forEach((it) -> {
+            Question q = it.into(Question.class);
+            Answer a = it.into(Answer.class);
+            if (a.getIsCorrect() == 1) correct.add(q); else incorrect.add(q);
+        });
+
+        // We need to copy the set.                   vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+        Set<Question> questions = correct.elementSet().stream().collect(Collectors.toSet());
+        questions.addAll(incorrect.elementSet());
+
+        return questions.stream()
+                .map(it -> new QuestionStatsData(it, correct.count(it), incorrect.count(it)))
+                .collect(Collectors.toList());
+    }
+
+    // DATA CLASSES
+    // Seriously, Java, why don't you have a thing for this bullshit? Kotlin and Scala do. </rant>
+    @SuppressWarnings("WeakerAccess")
+    private static class QuizStatsData {
+        public final Quiz quiz;
+        public final List<QuestionStatsData> questions;
+        public final int timesAttempted;
+        public final int uniqueStudentsAttempted;
+
+        public QuizStatsData(Quiz quiz, List<QuestionStatsData> questions, int timesAttempted,
+                             int uniqueStudentsAttempted) {
+            this.quiz = quiz;
+            this.questions = questions;
+            this.timesAttempted = timesAttempted;
+            this.uniqueStudentsAttempted = uniqueStudentsAttempted;
+        }
+    }
+
+    @SuppressWarnings("WeakerAccess")
+    private static class QuestionStatsData {
+        public final Question question;
+        public final int rightAnswers;
+        public final int wrongAnswers;
+
+        public QuestionStatsData(Question question, int rightAnswers, int wrongAnswers) {
+            this.question = question;
+            this.rightAnswers = rightAnswers;
+            this.wrongAnswers = wrongAnswers;
+        }
     }
 
 }
