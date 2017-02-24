6a459fea93d82883bf281a5c4f49696f45509787
RONAN-HP\ronan <rljoy@dundee.ac.uk>
2017-02-24 10:20:03 +0000

    Button created for viewing results as staff member.

---
+++ src/main/resources/public/staff/viewResult.js
@@ -0,0 +1,19 @@
+/* 
+ * Created by Ronan Joy + Kerr Mc
+ */
+
+
+function startQuiz(){
+
+    //display results
+    document.getElementById("resultContainer").style.display = "block";
+
+}
+
+function goBack() {
+    
+    //Button goes back to previous page, or home. 
+    window.alert("goBack() has been called.");
+    
+}
+
